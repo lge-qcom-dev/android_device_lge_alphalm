e39a05b6773338c1bde2ea3ba1d4c9c4f4611692
Tuan Anh <tuan73176@gmail.com>
2024-08-27 07:42:54 +0700

    alphalm: Commonize barometer sensor permissions
    All msmnile LGEs have a barometer sensor.
    
    Change-Id: If68f254d98116b99dea105036c12e0a2ce0dc29e

--- device.mk
+++ device.mk
@@ -53,7 +53,6 @@ PRODUCT_COPY_FILES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.hifi_sensors.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.hifi_sensors.xml
 
 # Shipping API Level
