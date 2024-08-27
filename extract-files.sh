7c2af05457b71af28c37f56d3221ee583e3b41c9
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 22:05:28 +0300

    alphalm: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
 function blob_fixup() {
     case "${1}" in
         vendor/lib/libSNPE.so|vendor/lib*/liblgdnnsnpe.so|vendor/lib*/liblgsnpeawb.so|vendor/lib*/liblgsnpeawb_beta.so|vendor/lib*/libtof_cal.so)
+            [ "$2" = "" ] && return 0
             ${PATCHELF_0_17_2} --replace-needed libstdc++.so libstdc++_vendor.so "${2}"
             ;;
+        *)
+            return 1
+            ;;
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # If we're being sourced by the common script that we called,
@@ -25,5 +34,6 @@ set -e
 export DEVICE=alphalm
 export DEVICE_COMMON=sm8150-common
 export VENDOR=lge
+export VENDOR_COMMON=${VENDOR}
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
